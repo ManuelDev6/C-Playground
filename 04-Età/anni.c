708757c60a573f51c4ca44772d27d1c3d9d69534
ManuelDev <manuelcacciamani1@gmail.com>
2026-08-18 18:30:17 +0200

    Add files via upload

---
+++ 04-Età/anni.c
@@ -0,0 +1,21 @@
+// Includo il file di intestazione
+
+#include <stdio.h>
+
+// Funzione principale
+
+int main() {
+	
+	int anni;
+	
+	// Inserimento anni
+	
+	printf("Quanti anni hai? ");
+	scanf("%d", &anni);
+	
+	// Stampa gli anni su schermo
+	
+	printf("Hai %d anni!", anni);
+	return 0;
+	
+}
