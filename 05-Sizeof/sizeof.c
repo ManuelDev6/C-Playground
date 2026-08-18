8b2998fcc4676fa080d8e339de9b87a4e9cade44
ManuelDev <manuelcacciamani1@gmail.com>
2026-08-18 18:35:16 +0200

    Add files via upload

---
+++ 05-Sizeof/sizeof.c
@@ -0,0 +1,16 @@
+// Includo il file di intestazione
+
+#include <stdio.h>
+
+// Funzione principale
+
+int main() {
+	
+	// Utilizzo della funzione sizeof con tipo int
+	
+	printf("Una variabile int occupa: ");
+	printf("%d byte", sizeof(int));
+	
+	return 0;
+	
+}
