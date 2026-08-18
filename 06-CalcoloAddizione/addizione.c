9f1b304abddcafd37774579c8ecef3001ab07051
ManuelDev <manuelcacciamani1@gmail.com>
2026-08-18 18:43:16 +0200

    Add files via upload

---
+++ 06-CalcoloAddizione/addizione.c
@@ -0,0 +1,35 @@
+// Includo il file di intestazione
+
+#include <stdio.h>
+
+// Funzione principale
+
+int main() {
+	
+	// Variabili utili al programma
+	
+	int num1;
+	int num2;
+	
+	int result;
+	
+	printf("CalcolaAddizione\n\n");
+	
+	// Inserimento primo numero
+	
+	printf("Inserisci il primo numero: ");
+	scanf("%d", &num1);
+	
+	// Inserimento secondo numero
+	
+	printf("Inserisci il secondo numero: ");
+	scanf("%d", &num2);
+	
+	// Stampa il risultato
+	
+	result = num1 + num2;
+	printf("\nRisultato: %d", result);
+	
+	return 0;
+	
+}
