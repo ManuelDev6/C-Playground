bb853bc185ac78a6b8515c9de54cc2cd09a40dde
ManuelDev <manuelcacciamani1@gmail.com>
2026-08-18 17:33:17 +0200

    Add files via upload

---
+++ 03-Carattere/carattere.cpp
@@ -0,0 +1,21 @@
+// Includo il file di intestazione
+
+#include <stdio.h>
+
+// Funzione principale
+
+int main() {
+	
+	char ch;
+	
+	// Inserimento carattere
+	
+	printf("Inserisci un carattere: ");
+	scanf("%c", &ch);
+	
+	// Stampa il carattere inserito
+	
+	printf("\nCarattere inserito: %c", ch);
+	return 0;
+	
+}
