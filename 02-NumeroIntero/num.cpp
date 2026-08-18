6e33aa586cf573f170301736767b0a3a77df1815
ManuelDev <manuelcacciamani1@gmail.com>
2026-08-18 17:30:26 +0200

    Add files via upload

---
+++ 02-NumeroIntero/num.cpp
@@ -0,0 +1,21 @@
+// Includo il file di intestazione
+
+#include <stdio.h>
+
+// Funzione principale
+
+int main() {
+	
+	int num;
+	
+	// Inserimento numero
+	
+	printf("Inserisci un numero intero: ");
+	scanf("%d", &num);
+	
+	// Stampa a schermo il numero inserito
+	
+	printf("\nNumero inserito: %d", num);
+	return 0;
+	
+}
